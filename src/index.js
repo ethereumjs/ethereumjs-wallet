818464229507827335a0e47232156abbc4af40f2
Chris <chris.eddy.dev@gmail.com>
2019-06-24 02:10:40 -0700

    update thirdparty.js scrypto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ var Buffer = require('safe-buffer').Buffer
 var ethUtil = require('ethereumjs-util')
 var crypto = require('crypto')
 var randomBytes = require('randombytes')
-var crypto = require('crypto')
 var uuidv4 = require('uuid/v4')
 var bs58check = require('bs58check')
 
